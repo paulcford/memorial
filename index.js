fb499e8cfcb3b5593e3f4c17e1f9e7d01cf0a841
Vita <vitanewstetter@ucla.edu>
2018-05-08 16:54:18 -0400

    update loading

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 
+var num = 12;
+
+var load = 0;
+
+window.onload = function(){
+	createFlickity();
+	
+};
+
+function createFlickity(){
+	var elem = ".slideshow-" + load;
+	var children = document.querySelector(elem).children;
+	var childrenArray = [...children];
+	childrenArray.forEach(function(slide){
+		slide.setAttribute('src', slide.dataset.src);
+	});
+	imagesLoaded(document.querySelector(elem), function(){
+		document.querySelector(elem).style.display = "block";
+		var test = new Flickity ( elem, {
+			cellSelector: ".slide",
+			wrapAround: true,
+			cellAlign: "center",
+			fullscreen: true,
+			autoPlay: 3000,
+			lazyLoad: 2,
+			imagesLoaded: true,
+			on: {
+				ready: function() {
+					console.log("Flickity ready: " + load);
+					if (load < num) {
+						load++;
+						createFlickity()
+					}
+				}
+			}
+		});
+	});
+}
